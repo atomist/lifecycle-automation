447e5a9b72e9afd3380790355d4e4a2f5a15f70e
Christian Dupuis <cd@atomist.com>
2018-10-05 15:44:58 +0200

    Update EnableGitHubPullRequestAutoMerge.ts

--- lib/handlers/command/github/EnableGitHubPullRequestAutoMerge.ts
+++ lib/handlers/command/github/EnableGitHubPullRequestAutoMerge.ts
@@ -65,7 +65,7 @@ export class EnableGitHubPullRequestAutoMerge implements HandleCommand {
     public githubToken: string;
 
     public async handle(ctx: HandlerContext): Promise<HandlerResult> {
-        await addAutoMergeLabels(ctx);
+        await addAutoMergeLabels(this.githubToken, this.apiUrl, ctx);
 
         const api = github.api(this.githubToken, this.apiUrl);
         await api.issues.addLabels({
